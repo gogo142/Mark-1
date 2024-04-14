92cee38dda7d769eee259812039d51e837733d45
George Manaz <george@MacBook-Air-som-tillhor-George.local>
2024-04-14 23:15:31 +0200

    added mongodb

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose=require("mongoose");
 app.use(express.json())
+
+
+const mongoUrl="mongodb+srv://georgemanazz:familj00@cluster0.c8jcwzx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(mongoUrl,{
+    useNewUrlParser: true,
+})
+.then(() => {
+    console.log("Connected to database");
+})
+.catch((e) => console.log(e))
+
 
 app.listen(5000,()=>{
     console.log('Server Started');
